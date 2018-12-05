7dbf27bb6c3793a897e575abb98a7696bc7c420a
jeff18ms <jeffrey.d.vasquez@gmail.com>
2018-12-05 11:47:09 +0800

    uncomment cp command

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,8 +34,8 @@ fi
 # Purge of confluence home:
 # https://confluence.atlassian.com/confkb/confluence-does-not-start-due-to-there-may-be-a-configuration-problem-in-your-confluence-cfg-xml-file-241568568.html
 # rm -rf ${CONFLUENCE_HOME}/*
-#rm -rf ${CONFLUENCE_HOME}/confluence.cfg.xml
-#cp ${CONFLUENCE_INSTALL_DIR}/confluence/WEB-INF/confluence-cfg/confluence.cfg.xml ${CONFLUENCE_HOME}/confluence.cfg.xml
+rm -rf ${CONFLUENCE_HOME}/confluence.cfg.xml
+cp ${CONFLUENCE_INSTALL_DIR}/confluence/WEB-INF/confluence-cfg/confluence.cfg.xml ${CONFLUENCE_HOME}/confluence.cfg.xml
 # Start Confluence as the correct user
 if [ "${UID}" -eq 0 ]; then
     echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
